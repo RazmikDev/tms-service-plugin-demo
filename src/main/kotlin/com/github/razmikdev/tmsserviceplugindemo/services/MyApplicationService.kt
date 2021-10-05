a8b49886ba7a3c2726644f7e0f2ce316a19c35e1
GitHub Action <action@github.com>
2021-10-05 18:08:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/razmikdev/tmsserviceplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.razmikdev.tmsserviceplugindemo.services
+
+import com.github.razmikdev.tmsserviceplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
